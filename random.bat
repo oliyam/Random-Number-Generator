f304b3a3713c3b1caaf430e00df6040a9f2a94a1
jyameogo <55655956+oliyam@users.noreply.github.com>
2022-10-10 22:17:45 +0200

    Update random.bat

--- random.bat
+++ random.bat
@@ -11,21 +11,26 @@ echo 2022-10-09
 echo:
 
 :options
+
+set b_=
+set a_=
+set o=
+
 echo Generate a random number between ...
 set /p o=zero and another number. [ENTER] two bounds. [b]
 
 :a
-set /p b_=A number:
+set /p b_=Bound:
 IF [%b_%]==[] (cls & echo Please provide a Number & goto a)
 IF %b_% NEQ +%b_% (cls & echo Please provide a positive Integer & goto a)	
 
-set a_=0
+:b
 IF "%o%"=="b" (
-	:b
-	set /p a_=Another number:
-	IF [%a_%]==[] (cls & echo Please provide a Number & goto b)
-	IF %a_% NEQ +%a_% (cls & echo Please provide a positive Integer & goto b)
-)
+	set /p a_=2nd Bound:
+) ELSE set a_=0
+
+IF [%a_%]==[] (cls & echo Please provide a Number & goto b)
+IF %a_% NEQ +%a_% (cls & echo Please provide a positive Integer & goto b)
 
 IF %a_% GTR %b_% (
 	set min=%b_%
@@ -38,6 +43,7 @@ IF %a_% GTR %b_% (
 
 :result
 
+cls
 set /a r=(%random%*(%max%-%min%+1)/32768) + %min%
 echo %r%
 echo:
