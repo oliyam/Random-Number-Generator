072e3a4f5d5dfdef3517894c821e81d5e75dbc9a
jyameogo <55655956+oliyam@users.noreply.github.com>
2022-10-10 02:27:37 +0200

    Update random.bat

--- random.bat
+++ random.bat
@@ -1,27 +1,55 @@
 @echo off
 color a
-echo #######################
-echo #### Best RNG ever ####
-echo #######################
+
+echo ##############################################################
+echo #### Der beste Zufallszahlengenerator diesseits der Donau ####
+echo ##############################################################	
+echo:
+echo Autor: oliver
+echo Version: 4.2.0
+echo 2022-10-09
 echo:
 
+:options
+echo Generate a random number between ...
+set /p o=zero and another number. [ENTER] two bounds. [b]
+
 :a
-set "min="
-set /p min=Bound 1:
-IF [%min%]==[] (echo Notnumerical! & goto a)
-IF %min% NEQ +%min% (echo Notnumerical! & goto a)
+set /p b_=A number:
+IF [%b_%]==[] (cls & echo Please provide a Number & goto a)
+IF %b_% NEQ +%b_% (cls & echo Please provide a positive Integer & goto a)	
 
-:b
-set "max="
-set /p max=Bound 2:
-IF [%max%]==[] (echo Notnumerical! & goto b)
-IF %max% NEQ +%max% (echo Notnumerical! & goto b)
+set a_=0
+IF "%o%"=="b" (
+	:b
+	set /p a_=Another number:
+	IF [%a_%]==[] (cls & echo Please provide a Number & goto b)
+	IF %a_% NEQ +%a_% (cls & echo Please provide a positive Integer & goto b)
+)
 
-:c
-set /a r=(%random%*(%max%-%min%)/32768) + %min%
+IF %a_% GTR %b_% (
+	set min=%b_%
+	set max=%a_%
+) ELSE (
+	set min=%a_%
+	set max=%b_%
+)
+
+
+:result
+
+set /a r=(%random%*(%max%-%min%+1)/32768) + %min%
 echo %r%
+echo:
 
 set /p again=again? [ENTER] new? [n]
-IF "%again%"=="n" (set "again=" & goto a) ELSE (goto c)
+IF "%again%"=="n" (
+	cls & set "again=" & goto options
+) ELSE IF "%again%"=="o" (
+	cls & set "again=" & goto options
+) ELSE (
+	goto result
+)
+
 
 pause
